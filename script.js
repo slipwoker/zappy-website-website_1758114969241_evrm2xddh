5f302e1429e4e9f4586d45137aa1fb17dcc15567
Maor Sharon <32263535+slipwoker@users.noreply.github.com>
2025-09-17 16:16:12 +0300

    Initial website deployment

---
+++ script.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', function() {document.querySelector('form').addEventListener('submit', function(e) {e.preventDefault();alert('Thank you for your message! We will get back to you soon.');});});
